f5638b626005e9102ae84ae38fc5f93de60f2db1
yoelenriquez <yoel.enriquez.1995@gmail.com>
2020-08-05 17:24:16 -0300

    Creando proyecto de Spring Framework 4

--- src/main/java/com/bolsadeideas/springboot/form/app/cotroller/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/cotroller/FormController.java
@@ -1,20 +1,52 @@
 package com.bolsadeideas.springboot.form.app.cotroller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 	
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Juan");
+		usuario.setApellido("Carlos");
+		usuario.setIdentificador("123.432-L");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Formulario usuarios");
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String procesar(Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
+		
+		
+		model.addAttribute("titulo", "Mostrando usuario");
+		
+		if(result.hasErrors()) {
+			/*Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(erro ->{
+				errores.put(erro.getField(), "Error en ".concat(erro.getField()).concat(" ").concat(erro.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);*/
+			
+			return "form";
+		}
+		
+		model.addAttribute("usuario", usuario);
+		status.setComplete();
+		
 		return "resultado";
 	}
 }
---
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -0,0 +1,76 @@
+package com.bolsadeideas.springboot.form.app.models.domain;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Usuario {
+	private String identificador;
+
+	@NotEmpty(message="El usuario no puede ser vacio")
+	private String nombre;
+	
+	@NotEmpty
+	private String apellido;
+
+	@NotEmpty
+	@Size(min = 4,max = 8)
+	private String username;
+
+	@NotEmpty
+	private String password;
+
+	@NotEmpty(message="El correo no puede ser vacio")
+	@Email
+	private String email;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	
+	
+}
